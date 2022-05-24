19ed6fca4afd4fd4cd741af0ded978d867750521
Michael Litchard <michael.litchard@platonic.systems>
2022-05-24 16:15:10 +0000

    first commit

---
+++ src/Plutus/Contracts/NFT.hs
@@ -0,0 +1,14 @@
+module Plutus.Contracts.NFT () where
+
+import qualified Data.Map                   as Map
+import           Data.Text                  (Text)
+import           Data.Void                  (Void)
+import           Ledger                     hiding (mint)
+import           Ledger.Constraints         as Constraints
+import           Ledger.Value               as Value
+import           Plutus.Contract            as Contract
+import           Plutus.MintingPolicies.NFT
+import           Prelude
+
+type Schema = Endpoint "mint" [TokenName]
+
